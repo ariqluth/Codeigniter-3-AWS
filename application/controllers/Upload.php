4afc0d793c7977c2f50fb27fae7d711ecdfd3762
ariqluth <ttzluthfi@gmail.com>
2022-12-12 10:21:59 +0700

    AWS bucket berasil dynamic

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -1,4 +1,7 @@
 <?php
+require 'vendor/autoload.php';
+use Aws\S3\S3Client;
+use Aws\S3\Exception\S3Exception;
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Upload extends CI_Controller
@@ -76,12 +79,115 @@ class Upload extends CI_Controller
         $config['max_height']  		= 2000;
         $config['file_name'] 		= $image_name;
 
+        //  // Instantiate an Amazon S3 client.
+        //  $s3_Config = array (
+        //     'key'    => 'AKIAU65DXABT67RJ2FE5',
+        //      'secret' => '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'
+        //  );
+
+        //  $s3Client = new S3Client([
+        //     'version' => 'latest',
+        //     'region'  => 'ap-southeast-1',
+        //     'credentials' => [
+        //         'key'    => $s3_Config['key'],
+        //         'secret' => $s3_Config['secret'],
+        //     ]
+        // ]);
+
+        // $bucket = 'server-bucket2';
+        // $file_Path = "/assets/upload/images/".$image_name;
+        // $key = basename($file_Path);
+
+        // try {
+        //     $result = $s3Client->putObject([
+        //         'Bucket' => $bucket,
+        //         'Key'    => $key,
+        //         // 'SourceFilePath' => fopen($file_Path, 'r'),
+        //         'Body'   => fopen($file_Path, 'r') or die ('Cannot open file : '.$file_Path),
+        //         'ACL'    => 'public-read', // make file 'public'
+        //     ]);
+        //   $msg = 'File has been uploaded';
+        // } catch (Aws\S3\Exception\S3Exception $e) {
+        //     // $msg = 'File has been uploaded';
+        //     echo $e->getMessage();
+        // }
+
+        // disiap kann s3 
+
+        // / AWS Info
+        $bucketName = 'server-bucker2';
+        $IAM_KEY = 'AKIAU65DXABT6RPJO7VO';
+        $IAM_SECRET = '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';
+    
+        // Connect to AWS
+        try {
+            // You may need to change the region. It will say in the URL when the bucket is open
+            // and on creation. us-east-2 is Ohio, us-east-1 is North Virgina
+            echo 1;
+            $s3 = S3Client::factory(
+                array(
+                    'credentials' => array(
+                        'key' => $IAM_KEY,
+                        'secret' => $IAM_SECRET
+                    ),
+                    'version' => 'latest',
+                    'region'  => 'ap-southeast-1'
+                )
+            );
+            
+        } catch (Exception $e) {
+            
+        die("Error: " . $e->getMessage());
+        }
+    
+        // For this, I would generate a unqiue random string for the key name. But you can do whatever.
+        $keyName = 'test_example/' . basename($_FILES["image"]['tmp_name']);   //ftp is file name at index.php
+        $pathInS3 = 'https://s3.ap-southeast-1.amazonaws.com/' . $bucketName . '/' . $keyName;
+    
+        // Add it to S3
+        try {
+            
+            if (!file_exists('assets/upload/images/'.$image_name)) {
+                echo 3;
+                mkdir('assets/upload/images/'.$image_name);
+            }
+                
+            $tempFilePath = 'assets/upload/images/'.$image_name. basename($_FILES["image"]['name']);
+         
+            $tempFile = fopen($tempFilePath, "w") or die("Error: Unable to open file.");
+            
+            $fileContents = file_get_contents($_FILES["image"]['tmp_name']);
+            $tempFile = file_put_contents($tempFilePath, $fileContents);
+    
+            $s3->putObject(
+                array(
+                    'Bucket'=>$bucketName,
+                    'Key' =>  $keyName,
+                    'SourceFile' => $tempFilePath,
+                    'StorageClass' => 'REDUCED_REDUNDANCY',
+                    'ACL'   => 'public-read'
+                )
+            );
+    
+        } catch (S3Exception $e) {
+            die('Error:' . $e->getMessage());
+        } catch (Exception $e) {
+            die('Error:' . $e->getMessage());
+        }
+    
+    
+        echo 'Done';
+    
+
         $this->load->library('upload', $config);
+       
+
         if (!$this->upload->do_upload('image')) {
             $this->session->set_flashdata('msg', $this->upload->display_errors('', ''));
             redirect('');
         }
         return $this->upload->data('file_name');
+        // $msg = 'File has been uploaded';
     }
 
     public function detail($id)
@@ -99,6 +205,8 @@ class Upload extends CI_Controller
         $this->Upload_model->delete($id);
         redirect('');
     }
+   
+
 }
 
 /* End of file Upload.php */
